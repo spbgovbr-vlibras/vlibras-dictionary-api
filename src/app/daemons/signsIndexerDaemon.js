a58bea52f57ec129895902a9b4680b82a6d6a060
Wesnydy Ribeiro <wesnydy@gmail.com>
2019-11-08 11:56:39 -0300

    Adding signs indexer daemon

---
+++ src/app/daemons/signsIndexerDaemon.js
@@ -0,0 +1,20 @@
+import env from '../../config/environments/environment';
+import { indexerError } from '../util/debugger';
+import { indexSigns } from '../util/signsIndexer';
+
+const signsIndexerDaemon = async function dictionarySignsIndexerDaemon() {
+  try {
+    await indexSigns();
+    setInterval(async () => {
+      try {
+        await indexSigns();
+      } catch (error) {
+        indexerError(error.message);
+      }
+    }, env.SIGNS_LIST_UPDATE_INTERVAL);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export default signsIndexerDaemon;
