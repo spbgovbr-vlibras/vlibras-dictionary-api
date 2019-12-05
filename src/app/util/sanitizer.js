a978ea8ce4cd6ce018dc631d982e90a385648926
Wesnydy Ribeiro <wesnydy@gmail.com>
2019-12-05 16:10:04 -0300

    Updating repository request module and adding context/ntensification sanitizers

--- src/app/util/sanitizer.js
+++ src/app/util/sanitizer.js
@@ -1,5 +1,23 @@
 const toUpperCaseSanitizer = function toUpperCaseCustomSanitizer(value) {
-  return typeof value === 'string' ? value.toUpperCase() : value;
+  return typeof value === 'string'
+    ? value.toUpperCase()
+    : value;
 };
 
-export default toUpperCaseSanitizer;
+const signIntensifierSanitizer = function signIntensifierCustomSanitizer(value) {
+  return typeof value === 'string'
+    ? value.replace(/([A-ZÁÉÍÓÚÀÂÊÔÃÕÜÇa-záéíóúàâêôãõüç]+)(\([-+]\))/, '$1')
+    : value;
+};
+
+const signContextSanitizer = function signContextCustomSanitizer(value) {
+  return typeof value === 'string'
+    ? value.replace(/([A-ZÁÉÍÓÚÀÂÊÔÃÕÜÇa-záéíóúàâêôãõüç]+)&([{A-ZÁÉÍÓÚÀÂÊÔÃÕÜÇa-záéíóúàâêôãõüç}]+)/, '$1')
+    : value;
+};
+
+export {
+  toUpperCaseSanitizer,
+  signIntensifierSanitizer,
+  signContextSanitizer,
+};
