ab0d23f2df3f7d5cacd2482d911109bbe490ce83
André <andre.araujo@lavid.ufpb.br>
2021-05-10 16:07:59 -0300

    Remove region

--- src/app/sign/signsDownloader.js
+++ src/app/sign/signsDownloader.js
@@ -153,7 +153,7 @@ const regionSignsDownloader = async function regionSignsDownloaderController(req
             req.params.version,
             req.params.platform,
             req.params.sign,
-            req.params.region,
+            // req.params.region,
           );
           res.download(signFile);
         } catch (repositorySignRequestError) {
@@ -178,7 +178,7 @@ const regionSignsDownloader = async function regionSignsDownloaderController(req
                   req.params.version,
                   req.params.platform,
                   alternativeSign,
-                  req.params.region,
+                  // req.params.region,
                 );
                 res.download(signFile);
               } catch (repositorySignRequestError2) {
